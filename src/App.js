baa0ff899a0c3e4bf8c9fe697d40e360829db498
kamal heydari <kamal3heydari@gmail.com>
2022-01-05 20:02:11 +0330

    update RRD to V6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 //? Components
 import { Navbar, Sidebar, Footer } from "./components";
