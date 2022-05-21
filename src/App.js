d51de8857d488f939565984beaf4992b33bf6008
kamal heydari <kamal3heydari@gmail.com>
2022-05-21 21:54:16 +0430

    feat globalstyles

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import { HashRouter as Router } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 import ConfigRoutes from "./routes/ConfigRoutes";
 
+//? Global Style
+import GlobalStyle from "./styles/GlobalStyle";
+
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <ConfigRoutes />
-      <Footer />
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <ConfigRoutes />
+        <Footer />
+      </Router>
+    </>
   );
 };
 
